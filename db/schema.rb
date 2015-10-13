43b808f68cd987f52e750de611bad12d3649c08a
patricksandquist <patricksandquist@gmail.com>
2015-10-13 14:31:32 -0700

    Try again

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151013171449) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
     t.string   "password_digest", null: false
