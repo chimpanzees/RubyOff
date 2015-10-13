13cb46709cac7c603a81bc960e37893e86641b45
patricksandquist <patricksandquist@gmail.com>
2015-10-13 14:58:38 -0700

    Remove javascript interpreter. Unsafe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      fail
       sign_in(@user)
       redirect_to sandboxes_url
     else
