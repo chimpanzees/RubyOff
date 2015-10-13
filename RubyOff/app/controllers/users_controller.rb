7e0c5fd7f0bb930039beadaef0b43983dd6685f1
patricksandquist <patricksandquist@gmail.com>
2015-10-13 10:54:45 -0700

    Add User model, controller, and Authentication

--- RubyOff/app/controllers/users_controller.rb
+++ RubyOff/app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      redirect_to sandbox_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
