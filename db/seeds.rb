d7fc491d45b8355421e1c7fdb462393b32036723
patricksandquist <patricksandquist@gmail.com>
2015-10-21 12:07:52 -0700

    Add seed data and lots of extra permissions

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,53 +9,75 @@ minh = User.create!(username: "MinhG-Man", password: "MinhDankWin")
 nathan = User.create!(username: "Nathan442", password: "HazeValley")
 nicole = User.create!(username: "LawyerNico", password: "j7df77af8s")
 patrick = User.create!(username: "HackerPat", password: "7asd9fsad987fadiu")
+gunit = User.create!(username: "GGG-Unit!!!", password: "7asd9sfadiu")
+
 question1 = Question.create!(
   title: "Doubler",
   question: "Write a class method that doubles an input number.",
   author_id: minh.id,
   solution_default: 'def self.double(num)
   # code goes here...
-end',
+end
+',
   tests_default: 'self.double(2) == 4
 self.double(1) == 2
-self.double(6) == 12'
+self.double(6) == 12
+'
 )
-question2 = Question.create!(
-  title: "Tripler",
-  question: "Write a class method that triples an input number.",
+
+sum_digits = Question.create!(
+  title: "Sum the digits",
+  question: "Given a string consisting of numbers, letters and symbols, write a function that will sum up the numbers.
+Note: consecutive integers should be treated as a single number. All numbers should be treated as positive integers as well.
+11-14 should be treated as 11 and 14.
+3.14 should be treated as 3 and 14.",
   author_id: nathan.id,
-  solution_default: 'def self.triple(num)
+  solution_default: 'def self.sum_from_string(str)
   # code goes here...
-end',
-  tests_default: 'self.triple(2) == 6
-self.triple(1) == 3
-self.triple(6) == 18'
+end
+',
+  tests_default: 'self.sum_from_string("In 2020, how much will an iPhone 18 cost?") == 2038
+self.sum_from_string("1+1=2") == 4
+self.sum_from_string("e=mc^2") == 2
+self.sum_from_string("aHR0cDovL3d3dy5jb2Rld2Fyy5jb20va2F0S9uF2wdA==") == 53
+self.sum_from_string("a30561ff4fb19170aa598b1431b52edad1fcc3e0") == 51820
+self.sum_from_string("x1KT   CmZ__\rYouOY8Uqu-ETtz") == 9
+self.sum_from_string("x1KT-8&*@\"CmZ__\rYouO  __Y8Uq\\u-ETtz") == 17
+self.sum_from_string("") == 0
+self.sum_from_string("Hello World") == 0
+'
 )
+
 powersOfTwo = Question.create!(
   title: "Powers of Two",
   question: "Write a class method that determines if given number is a power of two. A power of two means a number of the form 2^n where n is an integer, i.e. the result of exponentiation with number two as the base and integer n as the exponent. I.e. 1024 is a power of two: it 2^10.",
   author_id: nicole.id,
   solution_default: 'def self.power_of_two?(x)
   # code goes here
-end',
+end
+',
   tests_default: 'self.power_of_two?(2) == true
 self.power_of_two?(4096) == true
 self.power_of_two?(5) == false
 '
 )
+
 isSquare = Question.create!(
   title: "You're a Square!",
   question: "Given an integral number, determine if it's a square number. In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.",
   author_id: patrick.id,
   solution_default: 'def self.is_square?(x)
   false
-end',
+end
+',
   tests_default: 'self.is_square?(2) == false
 self.is_square?(1) == true
 self.is_square?(6) == false
 self.is_square?(25) == true
-self.is_square?(-1) == false'
+self.is_square?(-1) == false
+'
 )
+
 solution1 = Solution.create!(
   body: 'def self.double(num)
   num * 2
@@ -63,13 +85,15 @@ end',
   author_id: nathan.id,
   question_id: question1.id
 )
+
 solution2 = Solution.create!(
   body: 'def self.triple(num)
-  num * 3
+  str.scan(/\d+/).map(&:to_i).reduce(0, :+)
 end',
-  author_id: minh.id,
-  question_id: question2.id
+  author_id: gunit.id,
+  question_id: sum_digits.id
 )
+
 solution3 = Solution.create!(
   body: 'def self.is_square?(x)
   x < 0 ? false : Math.sqrt(x) % 1 == 0
@@ -77,6 +101,7 @@ end',
   author_id: minh.id,
   question_id: isSquare.id
 )
+
 solution4 = Solution.create!(
   body: 'def self.is_square?(x)
   x >= 0 && Math.sqrt(x).floor ** 2 == x
@@ -84,6 +109,7 @@ end',
   author_id: nicole.id,
   question_id: isSquare.id
 )
+
 solution5 = Solution.create!(
   body: 'def self.is_square?(x)
   return false if x < 0
@@ -93,6 +119,7 @@ end',
   author_id: nathan.id,
   question_id: isSquare.id
 )
+
 solution6 = Solution.create!(
   body: 'def self.is_square?(x)
   (0..x).each do |i|
@@ -107,6 +134,7 @@ end',
   author_id: patrick.id,
   question_id: isSquare.id
 )
+
 solution7 = Solution.create!(
   body: 'def self.power_of_two?(x)
   Math.log2(x) % 1 == 0
@@ -114,6 +142,7 @@ end',
   author_id: patrick.id,
   question_id: powersOfTwo.id
 )
+
 solution8 = Solution.create!(
   body: 'def self.power_of_two?(x)
   x.to_s(2).scan(/1/).length == 1
@@ -121,6 +150,7 @@ end',
   author_id: minh.id,
   question_id: powersOfTwo.id
 )
+
 solution9 = Solution.create!(
   body: 'def self.power_of_two?(x)
   while x%2==0 do
@@ -131,10 +161,46 @@ end',
   author_id: nathan.id,
   question_id: powersOfTwo.id
 )
+
 solution10 = Solution.create!(
   body: 'def self.power_of_two?(x)
   x == 1 || (x % 2).zero? && power_of_two?(x / 2)
 end',
   author_id: nicole.id,
   question_id: powersOfTwo.id
+)
+
+solution11 = Solution.create!(
+  body: 'def self.sum_from_string(str)
+  str.scan(/\d+/).map(&:to_i).inject(:+) || 0
+end',
+  author_id: patrick.id,
+  question_id: sum_digits.id
+)
+
+solution12 = Solution.create!(
+  body: 'def self.sum_from_string(str)
+  array = str.split("")
+  digits=["0","1","2","3","4","5","6","7","8","9"]
+  array.each do |x|
+    x.replace("_") if !(digits.include? x)
+  end
+  new_string = array.join
+  newer_string = (new_string.split("_"))
+
+  if newer_string.select{|x| x == "_"} == []
+    newer_string.delete_if{|x| x == "_"}
+  else
+  end
+
+  if newer_string == []
+    return 0
+  else
+  end
+
+  newer_string.map!{|x| x.to_i}
+  return newer_string.inject{|sum,n| sum + n}
+end',
+  author_id: nathan.id,
+  question_id: sum_digits.id
 )
