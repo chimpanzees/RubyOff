1d125491b3ab97dedffcf6e2599033c964f9d691
patricksandquist <patricksandquist@gmail.com>
2015-10-27 12:20:58 -0700

    Add demo login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,11 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in(@user)
-      redirect_to root_url
+      if params[:outcomeType] == "JS"
+        render :js => "window.location = '#{root_path}'"
+      else
+        redirect_to root_url
+      end
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
