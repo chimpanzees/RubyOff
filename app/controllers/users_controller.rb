a7fd39df8550fb6d467faad92c1350d3266cb61a
patricksandquist <patricksandquist@gmail.com>
2016-01-26 20:08:05 -0800

    Add demo user flag and Faker logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    # If this is a demo user
+    if user_params[:username] == "DEMO_USER_FLAG"
+      # Use Faker to create a fake username
+      demo_username = Faker::Internet.user_name
+      @user = User.new({:username => demo_username, :password => "alkdjfalsdj"})
+    else
+      @user = User.new(user_params)
+    end
     if @user.save
       sign_in(@user)
       if params[:outcomeType] == "JS"
